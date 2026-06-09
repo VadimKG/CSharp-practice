c38a818f0d26e067b6ecb66e50efbf915c35cf16
Vadim <vadim.kirilchik@gmail.com>
2026-06-09 20:10:33 +0200

    fix: final cleanup of nested repos and add real files

---
+++ KfcOrderSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿public static class PlayAnalyzer
+{
+    public static string Analyze(int shirtNum)
+    {
+        switch (shirtNum)
+        {
+            case 1:
+                return "Bucket of wings is ready";
+            case 2:
+                return "Twister is ready";
+            case 3:
+                return "Zinger is ready";
+            default:
+                return "Error!"
+        }
+    }
+}
