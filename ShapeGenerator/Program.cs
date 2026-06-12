521fe1656fe1fde9198a80ef664143d6abffe2a8
Vadim <vadim.kirilchik@gmail.com>
2026-06-12 16:30:52 +0200

    Task Shape Generator

---
+++ ShapeGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿int size = 5;
+
+for(int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
