c1b9f2662912e001df68baca3186a031145cdb7c
Vadim <vadim.kirilchik@gmail.com>
2026-06-18 02:20:15 +0200

    feat: Tracker solution

---
+++ ExpenseTracker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Solution(string str)
+    {
+        string result = "";
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            result += str[i];
+        }
+        return result;
+    }
+}
