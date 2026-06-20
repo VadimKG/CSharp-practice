0dbd30b50b16de201e4105e1a245d164132d8dc2
Vadim <vadim.kirilchik@gmail.com>
2026-06-20 02:32:22 +0200

    feat: add even or odd, reversed strings and string repeat

--- CodeWarsPractise/Program.cs
+++ CodeWarsPractise/Program.cs
@@ -4,6 +4,7 @@ namespace Solution
 {
     public class SolutionClass
     {
+        // 1. Even or Odd
         public static string EvenOrOdd(int number)
         {
             if (number % 2 == 0)
@@ -14,6 +15,28 @@ namespace Solution
             {
                 return ("Odd");
             }
+        }
+
+        // 2. Reversed Strings
+        public static string Solution(string str)
+        {
+            string result = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+
+        // 3. String repeat
+        public static string RepeatStr(int n, string s)
+        {
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                result += s;
+            }
+            return result;
         }
     }
 }
