1a2912efd4b5f2aa6d55539af8c67cb88d711cac
Vadim <vadim.kirilchik@gmail.com>
2026-06-21 01:27:06 +0200

    refactor: reorganize CodeWars tasks and add FindSmallestInteger

---
+++ CodeWarsPractise/FindSmallestInteger/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+    public static int FindSmallestInt(int[] args)
+    {
+        int min = args[0];
+        for (int i = 1; i < args.Length; i++)
+        {
+            if (args[i] < min)
+            {
+                min = args[i];
+            }
+        }
+        return min;
+    }
+}
