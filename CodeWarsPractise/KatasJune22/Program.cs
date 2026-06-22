6a54811af8cc46240e2f894a7614f4bfc6b5fc34
Vadim <vadim.kirilchik@gmail.com>
2026-06-22 23:46:27 +0200

    feat: solve convert to string and return negative katas

---
+++ CodeWarsPractise/KatasJune22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Kata
+    {
+        //Convert a String to a Number!
+
+        public static int StringToNumber(String str)
+        {
+            int.TryParse(str, out int number);
+            return number;
+        }
+
+        //Return Negative
+
+        public static int MakeNegative(int number)
+        {
+            return number > 0 ? -number : number;
+        }
+    }
+}
