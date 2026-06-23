41ef2083bc5720927d1273fa806266f18e6db81f
Vadim <vadim.kirilchik@gmail.com>
2026-06-23 22:12:04 +0200

    feat: solve counting sheep kata

---
+++ CodeWarsPractise/CountingSheep/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int CountSheeps(bool[] sheeps)
+    {
+        int count = 0;
+        for (int i = 0; i < sheeps.Length; i++)
+        {
+            if (sheeps[i] == true)
+            {
+                count += 1;
+            }
+
+            else
+            {
+                continue;
+            }
+        }
+        return count;
+    }
+}
