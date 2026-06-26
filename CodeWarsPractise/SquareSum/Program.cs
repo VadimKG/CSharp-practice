13b8c5bffa8dde0b3431f60ba66de855adf4a30e
Vadim <vadim.kirilchik@gmail.com>
2026-06-26 09:56:01 +0200

    feat: solve square sum kata

---
+++ CodeWarsPractise/SquareSum/Program.cs
@@ -0,0 +1,12 @@
+﻿public static class Kata
+{
+    public static int SquareSum(int[] numbers)
+    {
+        int result = 0;
+        foreach (int number in numbers)
+        {
+            result += number * number;
+        }
+        return result;
+    }
+}
