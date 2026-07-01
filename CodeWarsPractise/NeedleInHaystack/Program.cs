0bffcd8191c32c0a1b2a8b407d465d3237bc90a9
Vadim <vadim.kirilchik@gmail.com>
2026-07-01 13:45:12 +0200

    feat: solve needle in the haystack kata

---
+++ CodeWarsPractise/NeedleInHaystack/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+    public static string FindNeedle(object[] haystack)
+    {
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (object.Equals(haystack[i], "needle"))
+                return "found the needle at position " + i;
+        }
+        return "needle";
+    }
+}
