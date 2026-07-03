019dfe2e8b029e820208423501d66a4fdf77fb80
Vadim <vadim.kirilchik@gmail.com>
2026-07-03 22:24:14 +0200

    solve: Calculate average kata

---
+++ CodeWarsPractise/CalculateAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿class AverageSolution
+{
+    public static double FindAverage(double[] array)
+    {
+        double sum = 0;
+        double result = 0;
+        if (array.Length != 0)
+        {
+            //foreach (double number in array)
+            //{
+            //    sum += number;
+            //}
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            result = sum / array.Length;
+            return result;
+        }
+
+        else
+            return 0;
+    }
+}
