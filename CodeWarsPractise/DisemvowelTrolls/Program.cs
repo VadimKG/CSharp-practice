4cb1c875517c6140e4283920e8f94cd5cc5fca94
Vadim <vadim.kirilchik@gmail.com>
2026-07-08 01:21:42 +0200

    feat: solve disemvowel trolls kata

---
+++ CodeWarsPractise/DisemvowelTrolls/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        string new_str = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            switch (str[i])
+            {
+                case 'a':
+                case 'i':
+                case 'e':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'I':
+                case 'E':
+                case 'O':
+                case 'U':
+                    break;
+                default:
+                    new_str += str[i];
+                    break;
+            }
+        }
+        return new_str;
+
+        //string new_str = ""; 
+        //string vowels = "aeiouAEIOU";
+
+        //foreach(char letter in str)
+        //{
+        //if(!vowels.Contains(letter)) 
+        // {
+        //    new_str += letter;
+        // }
+        //}
+
+        // return new_str;
+    }
+}
