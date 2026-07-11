9ff154e638ea25aab1cfe1577fe1fd500741b79f
Vadim <vadim.kirilchik@gmail.com>
2026-07-11 23:26:25 +0200

    feat: solve exes and ohs kata

---
+++ CodeWarsPractise/ExesAndOhs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System;
+public static class Kata
+{
+    public static bool XO(string input)
+    {
+        int count_Oo = 0;
+        int count_Xx = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == 'o' || input[i] == 'O')
+                count_Oo++;
+            if (input[i] == 'x' || input[i] == 'X')
+                count_Xx++;
+        }
+
+        if (count_Oo == count_Xx)
+            return true;
+        else
+            return false;
+    }
+}
