081474d9c2ebb4fa9631f12343bc76109029599e
Vadim <vadim.kirilchik@gmail.com>
2026-07-13 02:31:28 +0200

    feat: solve shortest word kata

---
+++ CodeWarsPractise/ShortestWord/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Kata
+{
+    public static int FindShort(string s)
+    {
+
+        string[] words = s.Split(' ');
+        int result = words[0].Length;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (result > words[i].Length)
+                result = words[i].Length;
+        }
+
+        //foreach(string word in words)
+        //{
+        //    if(result > word.Length)
+        //        result = word.Length;  
+        //}
+        return result;
+    }
+}
