3cc6c640ed1f4ad0bb4d886dec43d03a2dbac6e0
Vadim <vadim.kirilchik@gmail.com>
2026-07-14 03:29:58 +0200

    feat: solve mumbling kata

---
+++ CodeWarsPractise/Mumbling/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Accumul
+{
+    public static string Accum(string s)
+    {
+        string sentence = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char up_letter = char.ToUpper(s[i]);
+            string low_letters = new string(char.ToLower(s[i]), i);
+            string row = up_letter + low_letters;
+            sentence += row + "-";
+        }
+        string result = sentence.TrimEnd('-');
+        return result;
+    }
+}
