2429f2e61759070539e4145d0841a1c81f88b694
Vadim Kirilchik <106678668+VadimKG@users.noreply.github.com>
2026-07-14 14:25:38 +0200

    Update Program.cs

--- CodeWarsPractise/InvertValues/Program.cs
+++ CodeWarsPractise/InvertValues/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+using System.Linq;
+namespace Solution
+{
+  public static class ArraysInversion
+  {
+    public static int[] InvertValues(int[] input)
+    {
+       for(int i = 0; i < input.Length; i++)
+       {
+            input[i] *= -1;
+       }
+      return input;
+    }
+  }
+}
