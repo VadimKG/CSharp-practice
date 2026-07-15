0ed0f97fd4e0e898ef239640ee8ed5a694997e17
Vadim <vadim.kirilchik@gmail.com>
2026-07-16 00:29:47 +0200

    feat: solve isograms kata

---
+++ CodeWarsPractise/Isograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+    public static bool IsIsogram(string str)
+    {
+        string l_str = str.ToLower();
+        char[] letter = l_str.ToCharArray();
+
+        for (int i = 0; i < letter.Length; i++)
+        {
+            for (int j = i + 1; j < letter.Length; j++)
+            {
+                if (letter[j] == letter[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
