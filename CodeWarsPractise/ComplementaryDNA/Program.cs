1ffdec69f87f03ba32443b490428290ef6178b14
Vadim <vadim.kirilchik@gmail.com>
2026-07-19 22:11:59 +0200

    feat: solve complementary dna kata

---
+++ CodeWarsPractise/ComplementaryDNA/Program.cs
@@ -0,0 +1,27 @@
+﻿public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        string result = dna
+          .Replace('T', '#')
+          .Replace('A', 'T')
+          .Replace('#', 'A')
+          .Replace('C', '#')
+          .Replace('G', 'C')
+          .Replace('#', 'G');
+        return result;
+
+        //char[] dnaChars = dna.ToCharArray();
+        //for (int i = 0; i < dnaChars.Length; i++)
+        //{
+        //    switch (dnaChars[i])
+        //    {
+        //        case 'A': dnaChars[i] = 'T'; break;
+        //        case 'T': dnaChars[i] = 'A'; break;
+        //        case 'C': dnaChars[i] = 'G'; break;
+        //        case 'G': dnaChars[i] = 'C'; break;
+        //    }
+        //}
+        //return new string(dnaChars);
+    }
+}
