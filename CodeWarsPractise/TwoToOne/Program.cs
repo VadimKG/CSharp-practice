be95f224429ab17d0c7572e1598a5a92c8c62ae4
Vadim <vadim.kirilchik@gmail.com>
2026-07-24 02:34:36 +0200

    feat: solve two to one kata with manual distinct and sort

---
+++ CodeWarsPractise/TwoToOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class TwoToOne
+{
+
+    public static string Longest(string s1, string s2)
+    {
+        string new_s = "";
+        string s3 = s1 + s2;
+        char[] letters_s3 = s3.ToCharArray();
+        Array.Sort(letters_s3);
+
+        for (int i = 0; i < letters_s3.Length - 1; i++)
+        {
+            if (letters_s3[i] == letters_s3[i + 1])
+                continue;
+
+            else
+                new_s += letters_s3[i];
+        }
+        new_s += letters_s3[letters_s3.Length - 1];
+        return new_s;
+    }
+}
