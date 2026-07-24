c4df3ee0a514257acc6aa07062c1170ef4945630
Vadim <vadim.kirilchik@gmail.com>
2026-07-24 22:17:30 +0200

    feat: solve reverse words kata manually

---
+++ CodeWarsPractise/ReverseWords/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string ReverseWords(string str)
+    {
+        string result = "";
+        string word = "";
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ' ')
+            {
+                result += word + " ";
+                word = "";
+            }
+            else
+            {
+                word = str[i] + word;
+            }
+        }
+        return result + word;
+    }
+}
