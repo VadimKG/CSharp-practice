df79596d25039d8ae5512652c573933236bf10ba
Vadim <vadim.kirilchik@gmail.com>
2026-07-28 16:34:36 +0200

    feat: solve validate pin code kata without regex

---
+++ CodeWarsPractise/ValidatePinCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static bool ValidatePin(string pin)
+    {
+        //return Regex.IsMatch(pin, @"^(\d{4}|\d{6})$");
+        if (pin.Length != 4 && pin.Length != 6)
+            return false;
+
+        for (int i = 0; i < pin.Length; i++)
+        {
+            if (!char.IsDigit(pin[i]) || pin[i] < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
