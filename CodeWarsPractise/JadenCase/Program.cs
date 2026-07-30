cde7a19857ceedb2a8da6ea3b9b6a5079a15b8fa
Vadim <vadim.kirilchik@gmail.com>
2026-07-30 14:34:25 +0200

    feat: solve jaden casing strings kata manually

---
+++ CodeWarsPractise/JadenCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        char[] phrase_mas = phrase.ToCharArray();
+        phrase_mas[0] = char.ToUpper(phrase_mas[0]);
+        for (int i = 0; i < phrase_mas.Length - 1; i++)
+        {
+            if (phrase_mas[i] == ' ')
+                phrase_mas[i + 1] = char.ToUpper(phrase_mas[i + 1]);
+        }
+        string result = new string(phrase_mas);
+        return result;
+    }
+}
