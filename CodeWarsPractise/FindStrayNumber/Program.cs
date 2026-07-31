6670cb341ce4ad48bee2a80287eb00bfdda67357
Vadim <vadim.kirilchik@gmail.com>
2026-07-31 15:27:41 +0200

    feat: solve find the stray number kata manually

---
+++ CodeWarsPractise/FindStrayNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿class Solution
+{
+    public static int Stray(int[] numbers)
+    {
+        if (numbers[0] != numbers[1])
+        {
+            if (numbers[0] != numbers[2])
+                return numbers[0];
+        }
+        if (numbers[0] != numbers[1])
+        {
+            if (numbers[1] != numbers[2])
+                return numbers[1];
+        }
+        if (numbers[0] == numbers[1])
+        {
+            if (numbers[1] != numbers[2])
+                return numbers[2];
+        }
+
+        for (int i = 3; i < numbers.Length; i++)
+        {
+            if (numbers[0] != numbers[i])
+                return numbers[i];
+        }
+        return numbers[0];
+    }
+}
