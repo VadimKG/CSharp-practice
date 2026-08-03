b3914551b5f3188c77e197666b5c280bc47c1a0a
Vadim <vadim.kirilchik@gmail.com>
2026-08-03 13:55:57 +0200

    feat: implement to-do list using dynamic List collection

---
+++ CSharpLabs/ToDoListLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace ToDoListLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> tasks = new List<string>();
+
+            Console.WriteLine("--- OLD TO DO LIST ---");
+            tasks.Add("Buy a cup of coffee");
+            tasks.Add("Learn C#");
+            tasks.Add("Drive a car");
+
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tasks[i]}");
+            }
+
+            tasks.Remove("Buy a cup of coffee");
+            tasks.Insert(0, "Learn List");
+
+            Console.WriteLine("\n--- NEW TO DO LIST ---");
+            for (int j = 0; j < tasks.Count; j++)
+            {
+                Console.WriteLine($"{j + 1}. {tasks[j]}");
+            }
+        }
+    }
+}
