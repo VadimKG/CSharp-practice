438c41cbe267f6b4e3ac7cff5d10f9ab8de22ff2
Vadim <vadim.kirilchik@gmail.com>
2026-08-05 14:34:48 +0200

    refactor: add constructor to enforce required data

--- CSharpLabs/KfcOrderLab/KfcOrder.cs
+++ CSharpLabs/KfcOrderLab/KfcOrder.cs
@@ -3,4 +3,11 @@ class KfcOrder
 	public string DishName;
 	public int Price;
 	public bool IsReady;
+
+    public KfcOrder(string inputDish, int inputPrice)
+    {
+        DishName = inputDish;
+        Price = inputPrice;
+        IsReady = false;
+    }
 }
