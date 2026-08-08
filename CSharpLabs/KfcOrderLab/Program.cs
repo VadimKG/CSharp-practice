c7cb54143019d931faf30feb4cf154379689b79c
Vadim <vadim.kirilchik@gmail.com>
2026-08-08 23:58:42 +0200

    feat: add DeliveryOrder class demonstrating inheritance

--- CSharpLabs/KfcOrderLab/Program.cs
+++ CSharpLabs/KfcOrderLab/Program.cs
@@ -7,13 +7,21 @@ namespace KfcOrderLab
         static void Main(string[] args)
         {
             KfcOrder myOrder = new KfcOrder("Bucket of Wings", 15);
-            myOrder.Price = -100;
+            //myOrder.Price = -100;
             Console.WriteLine("--- YOUR ORDER ---\n");
             Console.WriteLine($"Dish: {myOrder.DishName}");
             Console.WriteLine($"Price: {myOrder.Price}");
             Console.WriteLine($"Ready?: {myOrder.IsReady}");
 
             myOrder.ServeOrder();
+
+            DeliveryOrder myNewOrder = new DeliveryOrder("Warszawa", "Bucket of Wings", 15);
+            Console.WriteLine("\n--- YOUR NEW ORDER ---\n");
+            Console.WriteLine($"Adress: {myNewOrder.DeliveryAddress}");
+            Console.WriteLine($"Dish: {myNewOrder.DishName}");
+            Console.WriteLine($"Price: {myNewOrder.Price}");
+
+            myNewOrder.ServeOrder();
         }
     }
 }
