16fde6abdce9ffcf0a074fa929daec47d88e30bb
Vadim <vadim.kirilchik@gmail.com>
2026-08-18 23:10:21 +0200

    feat: add IChargeable interface and implement it in ElectricScooter

---
+++ TransportPractise/ElectricScooter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportPractise
+{
+    public class ElectricScooter : Transport, IChargeable
+    {
+        public int BatteryLevel { get; set; }
+
+        public ElectricScooter(string id, int maxZoneRadius, double pricePerMinute, int batteryLevel) : base(id, maxZoneRadius, pricePerMinute)
+        {
+            BatteryLevel = batteryLevel;
+        }
+
+        public override void Reserve()
+        {
+            if (BatteryLevel < 10)
+                Console.WriteLine("Battery level is too low.");
+            else
+                base.Reserve();
+        }
+
+        public void Charge()
+        {
+            BatteryLevel = 100;
+            Console.WriteLine("Battery is fully charge!");
+        }
+    }
+}
---
+++ TransportPractise/Transport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportPractise
+{
+    public abstract class Transport
+    {
+        public string ID { get; protected set; }
+
+        public int MaxZoneRadius { get; set; }
+
+        public double PricePerMinute { get; set; }
+
+        public Transport(string id, int maxZoneRadius, double pricePerMinute)
+        {
+            ID = id;
+            MaxZoneRadius = maxZoneRadius;
+            PricePerMinute = pricePerMinute;
+        }
+
+        public virtual void Reserve()
+        {
+            Console.WriteLine($"Transport {ID} had been reserved");
+        }
+    }
+}
