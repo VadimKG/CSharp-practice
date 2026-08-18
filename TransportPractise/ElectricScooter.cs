00500ffa55a6f3b3c89eac0e3d5f75a8e4b95b34
Vadim <vadim.kirilchik@gmail.com>
2026-08-18 22:22:18 +0200

    feat: base architecture for transport rental system

---
+++ TransportPractise/ElectricScooter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportPractise
+{
+    public class ElectricScooter : Transport
+    {
+        public int BatteryLevel { get; set; }
+
+        public ElectricScooter(string id, int maxZoneRadius, double pricePerMinute, int batteryLevel) : base(id, maxZoneRadius, pricePerMinute)
+        {
+            BatteryLevel = batteryLevel;
+        }
+
+        public override void Reserve()
+        {
+            if (BatteryLevel < 10)
+                Console.WriteLine("Battery level is too low.");
+            else
+                base.Reserve();
+        }
+    }
+}
---
+++ TransportPractise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TransportPractise;
+
+namespace TransportOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ElectricScooter scooter1 = new ElectricScooter("SC-001", 2500, 3.0, 100);
+            Bicycle bike1 = new Bicycle("BC-001", 5000, 1.0, 5);
+            
+            scooter1.Reserve();
+            bike1.Reserve();
+        }        
+    }
+}
