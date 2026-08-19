bb473e470fce016f8ff4cfb63ae0d43b079319f1
Vadim <vadim.kirilchik@gmail.com>
2026-08-19 23:18:31 +0200

    feat: implement polymorphism with List<Transport>

--- TransportPractise/Program.cs
+++ TransportPractise/Program.cs
@@ -10,8 +10,18 @@ namespace TransportOrder
             ElectricScooter scooter1 = new ElectricScooter("SC-001", 2500, 3.0, 100);
             Bicycle bike1 = new Bicycle("BC-001", 5000, 1.0, 5);
             
-            scooter1.Reserve();
-            bike1.Reserve();
-        }        
+            List<Transport> park = new List<Transport>
+            {
+                scooter1,
+                new ElectricScooter("SC-002", 2500, 3.0, 9),
+                bike1,
+                new Bicycle("BC-002", 5000, 1.0, 2),
+            };
+
+            foreach (Transport transport in park)
+            {
+                transport.Reserve();
+            }
+        } 
     }
 }
