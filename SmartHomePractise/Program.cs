4fa5f80a2d47854e161aad4e078c060a75c1b339
Vadim <vadim.kirilchik@gmail.com>
2026-09-01 22:08:55 +0200

    feat: implement abstract SmartDevice and polymorphism

--- SmartHomePractise/Program.cs
+++ SmartHomePractise/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace SmartHomePractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SmartDevice> device = new List<SmartDevice>()
+            {
+                new SmartSpeaker { Name = "Apple HomePode", VolumeLevel = 50},
+                new SmartLamp { Name = "Xiaomi Lamp", Brightness = 50}
+            };
+
+            foreach (var currentDevice in device)
+            {
+                currentDevice.TurnON();
+            }
+        }
+    }
+}
