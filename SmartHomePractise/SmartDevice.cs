50ced5461bceda88838a845539cc9b8b9eeadef4
Vadim <vadim.kirilchik@gmail.com>
2026-09-02 22:52:40 +0200

    feat: implement encapsulation, private set, and exception handling

--- SmartHomePractise/SmartDevice.cs
+++ SmartHomePractise/SmartDevice.cs
@@ -15,18 +15,31 @@ namespace SmartHomePractise
 
     class SmartSpeaker : SmartDevice
     {
-        public int VolumeLevel { get; set; }
+        public int VolumeLevel { get; private set; } = 50;
+        public void ChangeVolume(int newVolume)
+        {
+            if (newVolume > 100 || newVolume < 0)
+                throw new ArgumentException("The volume must be between 0% and 100%.");
+
+            VolumeLevel = newVolume;
+        }
+
         public override void TurnON()
         {
             IsOn = true;
-            Console.WriteLine($"Speaker \"{Name}\" is turned on. Volume: {VolumeLevel}/100");
+            Console.WriteLine($"Speaker \"{Name}\" is turned on. Volume: {VolumeLevel}/100%");
         }
     }
 
     class SmartLamp : SmartDevice
     {
         public string BulbType { get; set; } = "SOLHETTA";
-        public int Brightness { get; set; }
+        public int Brightness { get; private set; } = 80;
+        public void ChangeBrightness(int newBrightness)
+        {
+            if(newBrightness > 100 || newBrightness < 0)
+                throw new ArgumentException("The brightness must be between 0 and 100.");
+        }
         public override void TurnON()
         {
             IsOn = true;
