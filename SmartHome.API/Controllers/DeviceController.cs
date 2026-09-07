35d74925e41bf0e6cbe974f5c6a6b55c2a867b53
Vadim <vadim.kirilchik@gmail.com>
2026-09-08 01:34:06 +0200

    feat: add SmartDevice model with GET and POST endpoints

--- SmartHome.API/Controllers/DeviceController.cs
+++ SmartHome.API/Controllers/DeviceController.cs
@@ -1,15 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartHome.API.Models;
 
 namespace SmartHome.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]  
+    [Route("api/[controller]")]
     public class DeviceController : ControllerBase
     {
         [HttpGet]
-        public string GetString()
+        public ActionResult<IEnumerable<SmartDevice>> GetList()
         {
-            return "Server is running!";
+            return Ok(_devices);
         }
+
+
+        private static List<SmartDevice> _devices = new List<SmartDevice>()
+        {
+            new SmartDevice { ID = 1, Name = "Xiaomi Lamp", IsOn = false },
+            new SmartDevice { ID = 2, Name = "Apple Lamp", IsOn = true }
+        };
+
+        [HttpPost]
+        public ActionResult AddDevice(SmartDevice newDevice)
+        {
+            _devices.Add(newDevice);
+            return Created("", newDevice);
+        }
+
+
+
     }
+
 }
