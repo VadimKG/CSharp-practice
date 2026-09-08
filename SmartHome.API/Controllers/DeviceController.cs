78571f8ec9416f1037f24e80d766e7205dd807a7
Vadim <vadim.kirilchik@gmail.com>
2026-09-08 18:42:00 +0200

    feat: add GET by ID, PUT and DELETE endpoints to DeviceController

--- SmartHome.API/Controllers/DeviceController.cs
+++ SmartHome.API/Controllers/DeviceController.cs
@@ -13,6 +13,15 @@ namespace SmartHome.API.Controllers
             return Ok(_devices);
         }
 
+        [HttpGet("{id}")]
+        public ActionResult<SmartDevice> GetDevice(int id)
+        {
+            var device = _devices.FirstOrDefault(d => d.ID == id);
+            if (device == null)
+                return NotFound();
+            else
+                return Ok(device);
+        }
 
         private static List<SmartDevice> _devices = new List<SmartDevice>()
         {
@@ -27,8 +36,30 @@ namespace SmartHome.API.Controllers
             return Created("", newDevice);
         }
 
+        [HttpPut("{id}")]
+        public ActionResult UpdateDevice(int id, SmartDevice updatedDevice)
+        {
+            var up_device = _devices.FirstOrDefault(d => d.ID == id);
+            if (up_device == null)
+                return NotFound();
 
+            up_device.Name = updatedDevice.Name;
+            up_device.IsOn = updatedDevice.IsOn;
 
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteDevice(int id)
+        {
+            var device_d = _devices.FirstOrDefault(del => del.ID == id);
+            if (device_d == null)
+                return NotFound();
+            
+            _devices.Remove(device_d);
+            
+            return NoContent();
+        }
     }
 
 }
