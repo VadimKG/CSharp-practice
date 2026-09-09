60e1a35d093dff75143783eb0cea5d8222db1694
Vadim <vadim.kirilchik@gmail.com>
2026-09-09 11:03:47 +0200

    feat: add validation rules to SmartDevice model

--- SmartHome.API/Models/SmartDevice.cs
+++ SmartHome.API/Models/SmartDevice.cs
@@ -1,8 +1,12 @@
-﻿namespace SmartHome.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartHome.API.Models
 {
     public class SmartDevice
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "The device name is required!")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "The name must contain between 2 and 50 characters.")]
         public string Name { get; set; } = string.Empty;
         public bool IsOn { get; set; }
     }
