d33c75cbc22fe3565f1a68671a8c52dc579a2bbe
Vadim <vadim.kirilchik@gmail.com>
2026-09-22 10:39:04 +0200

    feat: solve sum of tow lowest 7 kyu with linq

---
+++ CodeWarsPractise/Sum of two lowest positive integers LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿public static class Kata
+{
+    public static int sumTwoSmallestNumbers(int[] numbers)
+    {
+        var sorted = numbers.Order();
+        return sorted.Take(2).Sum();
+
+        //int result = 0;
+
+        //for (int i = 0; i < numbers.Length; i++)
+        //{
+        //    for (int j = 0; j < numbers.Length - i - 1; j++)
+        //    {
+        //        if (numbers[j] > numbers[j + 1])
+        //        {
+        //            int temp = numbers[j];
+        //            numbers[j] = numbers[j + 1];
+        //            numbers[j + 1] = temp;
+        //        }
+        //    }
+        //}
+        //result = numbers[0] + numbers[1];
+        //return result;
+    }
+}
